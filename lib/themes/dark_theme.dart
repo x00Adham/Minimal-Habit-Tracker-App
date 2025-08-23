ce2d5314589f5b7c34b32e229374187b05add133
“Adham <adham.m.abdulah@gmail.com>
2025-08-23 15:45:48 +0300

    Refactor main app structure and update dependencies. Changed main app class from MyApp to HabitTracker, added light and dark theme files, and updated package versions in pubspec.lock.

---
+++ lib/themes/dark_theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkMode = ThemeData(
+  colorScheme: ColorScheme.dark(
+    surface: Colors.grey.shade900,
+    primary: Colors.grey.shade600,
+    secondary: Colors.grey.shade700,
+    tertiary: Colors.grey.shade800,
+    inversePrimary: Colors.grey.shade300,
+  ),
+  textTheme: ThemeData.dark().textTheme.apply(
+    bodyColor: Colors.grey.shade200,
+    displayColor: Colors.grey.shade200,
+  ),
+);
---
+++ lib/themes/light_theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  colorScheme: ColorScheme.light(
+    surface: Colors.grey.shade300,
+    primary: Colors.grey.shade500,
+    secondary: Colors.grey.shade200,
+    tertiary: Colors.white,
+    inversePrimary: Colors.grey.shade800,
+  ),
+  textTheme: ThemeData.light().textTheme.apply(
+    bodyColor: Colors.grey.shade800,
+    displayColor: Colors.grey.shade800,
+  ),
+);
